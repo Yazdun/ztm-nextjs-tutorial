781e50dde6c583d648a54e1b74f899373f2bc975
Yazdun <yazduntube@gmail.com>
2022-09-12 23:17:09 +0430

    commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,7 @@ import { getVideos } from "../lib/videos";
 import styles from "../styles/Home.module.css";
 
 export async function getServerSideProps() {
-  const disneyVideos = getVideos();
+  const disneyVideos = await getVideos();
   return { props: { disneyVideos } };
 }
 
