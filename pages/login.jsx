819a4926d29a24a57cc43f1ac055bb96b3cfda2b
Yazdun <yazduntube@gmail.com>
2022-09-13 14:46:06 +0430

    commit

---
+++ pages/login.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import React from "react";
+
+export default function Login() {
+  return (
+    <>
+      <Head>
+        <title>Netflix SignIn</title>
+      </Head>
+      <div>
+        <h1>login</h1>
+      </div>
+    </>
+  );
+}
