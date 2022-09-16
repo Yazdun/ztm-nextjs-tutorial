1882c79312498d1af8255e9fec3b60e4acb1229f
Yazdun <yazduntube@gmail.com>
2022-09-16 14:48:45 +0430

    commit

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,7 +1,25 @@
 import { useRouter } from "next/router";
+import Modal from "react-modal";
+import styles from "../../styles/Video.module.css";
+
+Modal.setAppElement("#__next");
 
 export default function Video() {
   const router = useRouter();
   console.log(router);
-  return <div>Video Page {router.query.videoId}</div>;
+  return (
+    <div>
+      Video Page {router.query.videoId}
+      <Modal
+        isOpen={true}
+        onRequestClose={() => {
+          router.back();
+        }}
+        contentLabel="Watch the video"
+        // overlayClassName={styles.overlay}
+      >
+        <div>Modal Body</div>
+      </Modal>
+    </div>
+  );
 }
