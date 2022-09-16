8fc8f6a4356c89d46c08f2a50a24407564ebd8ac
Yazdun <yazduntube@gmail.com>
2022-09-16 19:42:32 +0430

    commit

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,45 +1,141 @@
+import { useState, useEffect } from "react";
+
 import { useRouter } from "next/router";
 import Modal from "react-modal";
 import styles from "../../styles/Video.module.css";
-import cls from "classnames";
+
+import NavBar from "../../components/nav/navbar";
+import clsx from "classnames";
+
+import { getYoutubeVideoById } from "../../lib/videos";
+
+import Like from "../../components/icons/like-icon";
+import DisLike from "../../components/icons/dislike-icon";
 
 Modal.setAppElement("#__next");
 
-export default function Video() {
-  const router = useRouter();
+export async function getStaticProps(context) {
+  const videoId = context.params.videoId;
+  const videoArray = await getYoutubeVideoById(videoId);
 
-  const video = {
-    title: "Hi cute dog",
-    publishTime: "1990-01-01",
-    description:
-      "a big red dog that is super cute,can he get any bigger?a big red dog that is super cute,can he get any bigger?a big red dog that is super cute,can he get any bigger?a big red dog that is super cute,can he get any bigger?a big red dog that is super cute,can he get any bigger?a big red dog that is super cute,can he get any bigger?a big red dog that is super cute,can he get any bigger?a big red dog that is super cute,can he get any bigger?a big red dog that is super cute,can he get any bigger?a big red dog that is super cute,can he get any bigger?a big red dog that is super cute,can he get any bigger?",
-    channelTitle: "Pata Pic",
-    viewcount: 10000,
+  return {
+    props: {
+      video: videoArray.length > 0 ? videoArray[0] : {},
+    },
+    revalidate: 10, // In seconds
+  };
+}
+
+export async function getStaticPaths() {
+  const listOfVideos = ["mYfJxlgR2jw", "4zH5iYM4wJo", "KCPEHsAViiQ"];
+  const paths = listOfVideos.map((videoId) => ({
+    params: { videoId },
+  }));
+
+  return { paths, fallback: "blocking" };
+}
+
+const Video = ({ video }) => {
+  const router = useRouter();
+  const videoId = router.query.videoId;
+
+  const [toggleLike, setToggleLike] = useState(false);
+  const [toggleDisLike, setToggleDisLike] = useState(false);
+
+  const {
+    title,
+    publishTime,
+    description,
+    channelTitle,
+    statistics: { viewCount } = { viewCount: 0 },
+  } = video;
+
+  useEffect(() => {
+    const handleLikeDislikeService = async () => {
+      const response = await fetch(`/api/stats?videoId=${videoId}`, {
+        method: "GET",
+      });
+      const data = await response.json();
+
+      if (data.length > 0) {
+        const favourited = data[0].favourited;
+        if (favourited === 1) {
+          setToggleLike(true);
+        } else if (favourited === 0) {
+          setToggleDisLike(true);
+        }
+      }
+    };
+    handleLikeDislikeService();
+  }, [videoId]);
+
+  const runRatingService = async (favourited) => {
+    return await fetch("/api/stats", {
+      method: "POST",
+      body: JSON.stringify({
+        videoId,
+        favourited,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   };
 
-  const { title, publishTime, description, channelTitle, viewcount } = video;
+  const handleToggleDislike = async () => {
+    setToggleDisLike(!toggleDisLike);
+    setToggleLike(toggleDisLike);
+
+    const val = !toggleDisLike;
+    const favourited = val ? 0 : 1;
+    const response = await runRatingService(favourited);
+  };
+
+  const handleToggleLike = async () => {
+    const val = !toggleLike;
+    setToggleLike(val);
+    setToggleDisLike(toggleLike);
+
+    const favourited = val ? 1 : 0;
+    const response = await runRatingService(favourited);
+  };
 
   return (
     <div className={styles.container}>
+      <NavBar />
       <Modal
         isOpen={true}
-        onRequestClose={() => {
-          router.back();
-        }}
         contentLabel="Watch the video"
+        onRequestClose={() => router.back()}
         className={styles.modal}
         overlayClassName={styles.overlay}
       >
-        <iframe
-          id="player"
-          className={styles.videoPlayer}
-          type="text/html"
-          width="640"
-          height="390"
-          src={`http://www.youtube.com/embed/${router.query.videoId}?enablejsapi=1&origin=http://example.com&controls=0&rel=0`}
-          frameBorder="0"
-        ></iframe>
+        <div className={styles.frame}>
+          <iframe
+            id="ytplayer"
+            className={styles.videoPlayer}
+            type="text/html"
+            width="100%"
+            height="360"
+            src={`https://www.youtube.com/embed/${videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
+            frameBorder="0"
+          ></iframe>
 
+          <div className={styles.likeDislikeBtnWrapper}>
+            <div className={styles.likeBtnWrapper}>
+              <button onClick={handleToggleLike}>
+                <div className={styles.btnWrapper}>
+                  <Like selected={toggleLike} />
+                </div>
+              </button>
+            </div>
+            <button onClick={handleToggleDislike}>
+              <div className={styles.btnWrapper}>
+                <DisLike selected={toggleDisLike} />
+              </div>
+            </button>
+          </div>
+        </div>
         <div className={styles.modalBody}>
           <div className={styles.modalBodyContent}>
             <div className={styles.col1}>
@@ -48,13 +144,13 @@ export default function Video() {
               <p className={styles.description}>{description}</p>
             </div>
             <div className={styles.col2}>
-              <p className={styles.subText}>
-                <span className={styles.subTitleText}>Cast :</span>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>Cast: </span>
                 <span className={styles.channelTitle}>{channelTitle}</span>
               </p>
-              <p className={styles.subText}>
-                <span className={styles.subTitleText}>Cast :</span>
-                <span className={styles.channelTitle}>{viewcount}</span>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>View Count: </span>
+                <span className={styles.channelTitle}>{viewCount}</span>
               </p>
             </div>
           </div>
@@ -62,4 +158,6 @@ export default function Video() {
       </Modal>
     </div>
   );
-}
+};
+
+export default Video;
